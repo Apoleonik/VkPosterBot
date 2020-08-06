5f41fec653144b04cd2c27e294cb261880b79f7e
Apoleon <poleqqq@gmail.com>
2020-08-06 22:16:48 +0300

    add is_active and vk_channel_id field

--- create_db.sql
+++ create_db.sql
@@ -1,7 +1,9 @@
 CREATE TABLE channels (
     id integer primary key,
+    is_active boolean default false,
     telegram_channel varchar(255) NOT NULL,
     vk_channel varchar(255) NOT NULL,
+    vk_channel_id int default 0,
     last_post_id integer default 0,
     send_video_post boolean default false,
     send_video_post_text boolean default false,
