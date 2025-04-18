9c64477fa79e7d5c1227c022e86135ecf4d69552
e.ershov <e.ershov@bi.zone>
2025-04-18 23:09:21 +0300

    Add .env

--- config/settings.py
+++ config/settings.py
@@ -1,3 +1,5 @@
-TELEGRAM_TOKEN = ''
-VK_TOKEN = ''
-ADMIN_ID = 0
+import os
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN", "")
+VK_TOKEN = os.getenv("VK_TOKEN", "")
+ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
